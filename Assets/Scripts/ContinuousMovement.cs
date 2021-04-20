de704affe7a7da84d6b062507830c386981187ed
Chief08 <chief08af@gmail.com>
2021-04-20 03:51:58 +0300

    j34

--- Assets/Scripts/ContinuousMovement.cs
+++ Assets/Scripts/ContinuousMovement.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+
 public class ContinuousMovement : MonoBehaviour
 {
     public float speed = 1;
     public XRNode inputSource;
+    public float gravity = 9.81f;
+    public LayerMask groundLayer;
+    public float additionalHeight = 0.2f;
 
     private XRRig rig;
     private Vector2 inputAxis;
     private CharacterController character;
+    private float fallingSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +33,34 @@ public class ContinuousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CapsuleFollowHead();
+
+        // direction
+
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0 , inputAxis.y);
         character.Move(direction * Time.fixedDeltaTime * speed);
+
+        // gravity
+        if (GroundedCheck())
+            fallingSpeed = 0;
+        else
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    void CapsuleFollowHead()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2, capsuleCenter.z);
+    }
+
+    bool GroundedCheck() {
+
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasContact = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasContact;
     }
 }
